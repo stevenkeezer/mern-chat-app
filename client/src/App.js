9accd5920aad66d573a89873c4e77cdcd90926c7
stevenkeezer <stevengkeezer@gmail.com>
2021-01-06 19:19:24 -0800

    added react router and login and signup pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,57 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
-
-import { theme } from "./themes/theme";
-import Home from "./pages/Home";
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 import "./App.css";
 
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#4791db',
+      main: '#3a8dff',
+      dark: '#115293',
+    },
+    secondary: {
+      light: '#ffd95a',
+      main: '#f9a825',
+      dark: '#c17900',
+      contrastText: '#212121',
+    },
+    background: {
+      default: '#ffffff',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+    button: {
+      textTransform: 'none'
+    }
+  },
+  underline: {
+    color: 'red',
+    '&:before': {
+      border: '2px solid red'
+    }
+  }
+});
+
+theme = responsiveFontSizes(theme);
+
 function App() {
+  const [loggedIn, setLoggedIn] = React.useState(true)
+
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={Home} />
+        <Route exact path="/">
+          {loggedIn ? <Redirect to="/signup" /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+
       </BrowserRouter>
     </MuiThemeProvider>
   );
